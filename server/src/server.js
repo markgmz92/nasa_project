b994fbd3cba4e96e94b2d285834b3c15dff3d378
markgmz92 <markgmz92@gmail.com>
2022-06-21 01:24:29 +0800

    [NP - 002] Loading planet data

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,19 @@
 const http = require('http');
 const app = require('./app');
+const { loadPlanetData } = require('./models/planets.model');
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+async function startServer() {
+  await loadPlanetData();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
 
 // const express = require('express')
 
 // const app = express();
 // app.listen()
+startServer();
