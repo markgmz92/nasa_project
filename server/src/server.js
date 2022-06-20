a81f6e5096075d9a1a9fb3832ef7032d9150caf6
markgmz92 <markgmz92@gmail.com>
2022-06-20 18:56:31 +0800

    [NP - 001] Repo initialisation

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const app = require('./app');
+const PORT = process.env.PORT || 8000;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
+
+// const express = require('express')
+
+// const app = express();
+// app.listen()
