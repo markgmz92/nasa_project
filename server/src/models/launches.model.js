d4eeac2921a1e6b41e07544ee5a439fdd820e9ab
markgmz92 <markgmz92@gmail.com>
2022-06-23 18:57:06 +0800

    [NP - 011] POST data from the front-end

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -6,8 +6,8 @@ const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
-  launchData: new Date('December 27, 2030'),
-  destination: 'Kepler-442 b',
+  launchDate: new Date('December, 27, 2030'),
+  target: 'Kepler-442 b',
   customer: ['ZTM', 'NASA'],
   upcoming: true,
   success: true
